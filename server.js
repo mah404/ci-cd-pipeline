d345d59e7c0b6d01e471723b632908b4c1013485
mah404 <108659794+mah404@users.noreply.github.com>
2026-06-25 10:21:30 +0200

    added new files

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+res.send('HEllo from Docker!!!🚀')
+})
+
+app.listen(3000,()=>{
+
+console.log('Server Running on Port 3000')
+})
